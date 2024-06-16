6d2c55433d688857be22439be9447f8705f55336
Aleksandr Tochilin <alex8079@gmail.com>
2024-06-16 04:15:10 +0300

    disable eslint warnings and errors in browser for devserver, integrate .env file for env variables

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,9 @@ import { fileURLToPath } from 'url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
 import ESLintPlugin from 'eslint-webpack-plugin';
+import Dotenv from 'dotenv-webpack';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 let mode = '';
@@ -18,57 +20,71 @@ if (process.env.NODE_ENV === 'development') {
 
 console.log(mode + ' mode');
 
-export default {
-    entry: path.join(__dirname, 'src/index.js'),
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    devtool: "source-map",
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/, // Matches all CSS files
-                use: [
-                    mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader', // Injects styles into DOM
-                    'css-loader',   // Translates CSS to CommonJS modules
-                    'postcss-loader',
-                ],
-            },
-            {
-                test: /\.s[ac]ss$/i, // Matches all SCSS/Sass files with any extension (scss or sass)
-                use: [
-                    mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader', // Injects styles into DOM
-                    'css-loader',   // Translates CSS to CommonJS modules
-                    'sass-loader',  // Compiles Sass/SCSS to CSS
-                    'postcss-loader',
-                ],
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: './public/index.html' }),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css', // Output filename for extracted CSS
-            chunkFilename: 'css/[id].css', // Output filename for CSS chunks
-        }),
-        new CssMinimizerPlugin(),
-        new ESLintPlugin(),
-    ],
-    devServer: {
-        hot: true,
-        port: 3000,
-        open: true
-    }
+export default (env) => {
+    console.log(env)
 
+    return {
+        entry: path.join(__dirname, 'src/index.js'),
+        output: {
+            path: path.join(__dirname, 'build'),
+            filename: 'bundle.js'
+        },
+        resolve: {
+            extensions: ['.js', '.jsx']
+        },
+        devtool: "source-map",
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.css$/, // Matches all CSS files
+                    use: [
+                        mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+                        'css-loader',
+                        'postcss-loader',
+                    ],
+                },
+                {
+                    test: /\.s[ac]ss$/i, // Matches all SCSS/Sass files with any extension (scss or sass)
+                    use: [
+                        mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+                        'css-loader',
+                        'sass-loader',  // Compiles Sass/SCSS to CSS
+                        'postcss-loader',
+                    ],
+                },
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({ template: './public/index.html' }),
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].css', // Output filename for extracted CSS
+                chunkFilename: 'css/[id].css', // Output filename for CSS chunks
+            }),
+            new CssMinimizerPlugin(),
+            new Dotenv(),
+            new ESLintPlugin({
+                threads: true
+            }),
+        ],
+        devServer: {
+            hot: true,
+            port: 3000,
+            open: true,
+            client: {
+                logging: 'none',
+                overlay: false,
+            }
+        },
+        optimization: {
+            minimize: true,
+            minimizer: [new TerserPlugin()],
+        },
+    }
 }
